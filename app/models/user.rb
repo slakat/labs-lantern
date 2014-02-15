581738a181a7befd7569af34a37ccfa86c4cdfb6
Pietro R <pietrord@hotmail.com>
2014-02-14 22:15:25 -0300

    ahora si

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
 	
-	before_save :encrypt_password
+	has_secure_password
+  validates_presence_of :password, :on => :create
 	validates :username, presence: true, uniqueness: true
 	validates_presence_of :full_name
 	validates_presence_of :role
+
   
-  has_secure_password
 	
 end
