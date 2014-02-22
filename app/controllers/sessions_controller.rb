cfa6531b3bba9d88b4029f1e60592efef8f575d7
Pietro R <pietrord@hotmail.com>
2014-02-21 21:41:48 -0300

    brute attack protection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,23 @@ class SessionsController < ApplicationController
 
   def create
 	  user = User.find_by_username(params[:username])
-	  if user && user.authenticate(params[:password])
-	    session[:user_id] = user.id
-	    redirect_to root_url, :notice => "Logged in!"
+	  if user
+	  	if user.attempts>5
+				sleep 10
+		end
+	  	if user.authenticate(params[:password])
+	  		@numero=user.attempts
+	  		user.attempts=0
+	  		user.save
+	    	session[:user_id] = user.id
+	    	redirect_to root_url, :notice => "Logged in!"+ @numero.to_s
+		else
+			user.attempts=user.attempts+1
+			user.save
+			render "new"
+		end
 	  else
+	  	sleep 2
 	    flash.now.alert = "Invalid email or password"
 	    render "new"
 	  end
